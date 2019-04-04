0b3cbc1253d32ba9a7673f27a6bb1fddad8ff7a5
maxjacobson <max.jacobson@torchlms.com>
2019-04-03 23:28:48 -0600

    setup basic view

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,50 @@
-// We need to import the CSS so that webpack will load it.
-// The MiniCssExtractPlugin is used to separate it out into
-// its own CSS file.
-import css from "../css/app.css"
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-// webpack automatically bundles all modules in your
-// entry points. Those entry points can be configured
-// in "webpack.config.js".
-//
-// Import dependencies
-//
-import "phoenix_html"
+class App extends React.Component {
 
-// Import local files
-//
-// Local files can be imported directly using relative paths, for example:
-// import socket from "./socket"
+
+    render() {
+
+        return (
+            <div className="container-fluid">
+                <div className="py-5 text-center">
+                    <img className="d-block mx-auto mb-4" src="https://getdivvy.com/wp-content/themes/divvy-child/images/Divvy-Logo-Teal.png" alt="Logo" width="181" />
+                    <h2>Warehouse Item Locator</h2>
+                    <p className="lead">Please enter the X and Y coordinates for your item</p>
+                </div>
+
+                <div className="col-12">
+                    <form className="needs-validation" noValidate>
+                        <div className="row" style={{justifyContent: 'center'}}>
+                            <div className="col-sm-12 col-lg-4 mb-3">
+                                <div className="input-group">
+                                    <div className="input-group-prepend">
+                                        <span className="input-group-text">X</span>
+                                    </div>
+                                    <input type="number" className="form-control" id="xCoordinate" max="100000" min="0" required />
+                                    <div className="invalid-feedback" style={{width: '100%'}}>
+                                        The x coordinate required.
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-sm-12 col-lg-4 mb-3">
+                                <div className="input-group">
+                                    <div className="input-group-prepend">
+                                        <span className="input-group-text">Y</span>
+                                    </div>
+                                    <input type="number" className="form-control" id="yCoordinate" max="100000" min="0" required />
+                                    <div className="invalid-feedback" style={{width: '100%'}}>
+                                        The Y coordinate is required.
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('mountPoint'))
--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -13,7 +13,7 @@ module.exports = (env, options) => ({
     ]
   },
   entry: {
-      './js/app.js': ['./js/app.js'].concat(glob.sync('./vendor/**/*.js'))
+    './js/app.js': ['./js/app.js'].concat(glob.sync('./vendor/**/*.js'))
   },
   output: {
     filename: 'app.js',
@@ -31,7 +31,26 @@ module.exports = (env, options) => ({
       {
         test: /\.css$/,
         use: [MiniCssExtractPlugin.loader, 'css-loader']
-      }
+      },
+
+
+      {
+        test: /\.(jpg|png)$/,
+        use: {
+          loader: "url-loader",
+          options: {
+            limit: 25000,
+          },
+        },
+      },
+
+      {
+        test: /\.(jpg|png)$/,
+        use: {
+          loader: "file-loader"
+        },
+      },
+
     ]
   },
   plugins: [
